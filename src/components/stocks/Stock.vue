d6aca60115f1d24e7cff543e322184ea7a7ba0f8
Bishwajit Jha <ajitjha393@gmail.com>
2021-07-11 16:06:04 +0530

    Update(Stocks): Added Buy Stock btn

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -3,16 +3,25 @@
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
-          NAME
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
-          <input type="number" class="form-control" placeholder="Quantity" />
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Quantity"
+            v-model="quantity"
+          />
         </div>
         <div class="pull-right">
-          <button class="btn btn-success">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="isDisabled"
+          >
             Buy
           </button>
         </div>
@@ -20,3 +29,31 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  computed: {
+    isDisabled() {
+      return this.quantity <= 0 || !Number.isInteger(+this.quantity);
+    }
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        stockQuantity: this.quantity
+      };
+
+      console.log(order);
+      this.quantity = 0;
+    }
+  }
+};
+</script>
