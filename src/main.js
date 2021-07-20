13963636065cc51a169bccac711c3093ed741714
Bishwajit Jha <ajitjha393@gmail.com>
2021-07-20 19:50:55 +0530

    feat(App): Setup vue-resource plugin

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 
 import App from './App.vue';
 import { routes } from './routes/app-routes';
@@ -9,6 +10,10 @@ Vue.config.productionTip = false;
 
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://arth-stock-trader-default-rtdb.firebaseio.com/'
+
 const router = new VueRouter({
   mode: 'history',
   routes
